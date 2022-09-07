427534357706cd89a36d73d4265af1058e2fd3b9
dewindtk <dewindtk@student.ethz.ch>
2022-09-07 17:38:52 +0200

    split tasks

--- findArbitrage.js
+++ findArbitrage.js
@@ -3,4 +3,10 @@ const Web3 = require(`web3`)
 require("dotenv").config();
 const web3 = new Web3(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);
 
-
+async function findArbitrage(collection){ //Consider FSM with State change commands
+    let listings = await Promise.all(getOsListings(collection), getLrListings(collection), getXyListings(collection))
+    let OsListings = listings[0]
+    let LrListings = listings[1]
+    let XyListings = listings[2]
+    
+}
---
+++ sendArbitrage.js
@@ -0,0 +1,4 @@
+fs = require('fs')
+const Web3 = require(`web3`)
+require("dotenv").config();
+const web3 = new Web3(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);
